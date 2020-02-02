e3f6413bf531bff1f914f6f39f632de2b9471eda
Your Name <you@example.com>
2020-02-02 21:47:00 +0800

    基础server模块定义

---
+++ znet/server.go
@@ -0,0 +1,21 @@
+package znet
+
+type Server struct {
+	Name string
+	IPVersion string
+	IP string
+	Port int
+}
+
+
+func (s * Server) start(){
+
+}
+
+func (s * Server) stop(){
+
+}
+
+func (s * Server) service(){
+
+}
